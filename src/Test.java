527ca2f53ae570360cb639d51ee72426eb81cc0d
Avinash Patel <145204403+avinashp22@users.noreply.github.com>
2024-01-27 15:09:41 +0000

    commit2

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("my name is avi");
+        
+        System.out.println("my name is patel");
     }
 }
